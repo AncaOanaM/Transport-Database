6c713e56ce0d89a3f327b3baab9b7cd17646fc6d
Anca Oana <150436408+AncaOanaM@users.noreply.github.com>
2023-12-15 17:51:41 +0200

    Update create table route.sql

--- create table route.sql
+++ create table route.sql
@@ -5,6 +5,8 @@ create table route
 				RouteName varchar(50) null,
 				RouteTime tinyint null)
 
+
+-- inserare date in coloana de ID	
 DECLARE @Counter INT = 1
 WHILE @Counter <= 500
 BEGIN
@@ -13,26 +15,19 @@ BEGIN
     SET @Counter = @Counter + 1;
 END
 
---with CTE as (SELECT
---    t1.OfficeAdress + ' ' + t2.OfficeAdress AS CombinedValues
---FROM
---    customers t1
---JOIN
---    customers t2 ON t1.OfficeAdress <> t2.OfficeAdress)
---select distinct CombinedValues
---from CTE
-
-
-WITH CTE AS (
-    SELECT
-        t1.IdRoute,
-        t1.RouteName + ' ' + t2.OfficeAdress AS CombinedValues
-    FROM
-        route t1
-    JOIN
-        customers t2 ON t1.IdRoute = t2.IdCustomer AND t1.RouteName <> t2.OfficeAdress
-)
-UPDATE route
-SET RouteName = c.CombinedValues
-FROM route t1
-JOIN CTE c ON t1.IdRoute = c.IdRoute;
+--am adus orasele din definirea adresei clientilor, si am creat ruta: combiantie de 2 orase	
+with CTE1 as 
+	(SELECT distinct
+	t1.OfficeAdress + ' ' + t2.OfficeAdress AS CombinedValues
+	FROM
+    customers t1
+JOIN
+    customers t2 ON t1.OfficeAdress <> t2.OfficeAdress),
+CTE2 as (select 
+CombinedValues,
+ROW_NUMBER() OVER (ORDER BY CTE1.CombinedValues) AS Id
+from CTE1)
+UPDATE Route
+SET RouteName = C.CombinedValues
+FROM Route r
+JOIN CTE2 C ON r.IdRoute = c.ID 
