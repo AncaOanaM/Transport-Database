a4f8a22094fedefeebcf3533571a51920833f0c1
Anca Oana <150436408+AncaOanaM@users.noreply.github.com>
2024-02-20 12:59:06 +0200

    Add files via upload

--- create table shipments.sql
+++ create table shipments.sql
@@ -1,10 +1,12 @@
+use shipment
+go
+
 drop table if exists shipments
 go
 
 create table shipments
 					(Idshipment smallint primary key not null,
 					 Idvehicle smallint references vehicles(IdVehicle) not null,
-					 IdDriver tinyint references drivers(IdDriver) not null,
 					 IdOrder smallint references orders(IdOrder) not null,
 					 StartTime datetime not null,
 					 EndTime datetime null,
@@ -13,4 +15,3 @@ create table shipments
 					 DistanceTraveled smallint null,
 					 ReasonForDelay varchar(200) null)
 go
-
