43acbca76e106a228fd4daf0e7a337fbc18df4c7
Anca Oana <150436408+AncaOanaM@users.noreply.github.com>
2024-02-20 14:26:57 +0200

    Update create table drivers.sql

--- create table drivers.sql
+++ create table drivers.sql
@@ -1,3 +1,6 @@
+use shipment
+go
+
 drop table if exists drivers
 go
 create table drivers
@@ -15,4 +18,14 @@ insert into drivers(IdDriver,FirstName,LastName,Adress,PhoneNumber) values
 (5,'Suteu','Marius',11,'+40724523989')
 (6,'Popescu','Ghiocel',11,'+40724862100'),
 (7,'Florescu','Robert',11,'+40724523998'),
-(8,'Nastase','Valentin',11,'+40724123202')
+(8,'Nastase','Valentin',11,'+40724123202'),
+(9,'Bunea','Viorel',11,'+4072666779'),
+(10,'Condei','Mircea',11,'+40721243202'),
+(11,'Bulgarea','Costin',11,'+4079822174'),
+(12,'Moise','Dorin',11,'+40745676878'),
+(13,'Noro','Vasi',11,'+4072999555'),
+(14,'Matic','Constantin',11,'+4073555666'),
+(15,'Olimpus','George',11,'+40765444333'),
+(16,'Grigore','Mihai',11,'+40754666555'),
+(17,'Diamandi','Octavian',11,'+4074566777'),
+(18,'Dumitru','Ion',11,'+407876354')
